7e196e4af2b56c829f618bb9940b1ed2488e0f9f
AminPriadi <45525407+AminPriadi@users.noreply.github.com>
2019-01-05 12:12:43 +0700

    Add files via upload

---
+++ program perkalian tanpa operator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int p,q,hasil=0;
+    cout<<"masukan bilangan pertama ";
+    cin>>p;
+    cout<<"masukan bilangan kedua ";
+    cin>>q;
+    for(int r=1; r<=q; r++)
+    {
+        hasil+=q;
+    }
+    cout<<"hasil "<<hasil<<endl;
+}
